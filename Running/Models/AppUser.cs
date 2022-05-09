ba921c32e8ce436d2cc69448024c2796029179c0
bill <wcstrickland1@gmail.com>
2022-05-09 15:52:27 -0500

    models and db up and seeded

---
+++ Running/Models/AppUser.cs
@@ -0,0 +1,15 @@
+﻿namespace Running.Models
+{
+    public class AppUser 
+    {
+        public string Id { get; set; }
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public Address? Address { get; set; }
+
+        public ICollection<Club> Clubs { get; set; }
+
+        public ICollection<Race> Races { get; set; }
+
+    }
+}
---
+++ Running/Models/Race.cs
@@ -0,0 +1,19 @@
+﻿using Running.Data.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Running.Models
+{
+    public class Race
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+        public RaceCategory RaceCategory { get; set; }
+    }
+}
